3e0d295665020bb0d6dbff2ed3fad4775a0e4a4c
higorvital <h.vitall96@gmail.com>
2018-04-02 09:42:04 -0300

    Update crc.s

--- crc.s
+++ crc.s
@@ -1,17 +1,10 @@
 main:
-	movhi r2, %hi(3254779904)
-	
-	movi r3, 32767
-	addi r3, r3, 32767
-	addi r3, r3, 1
-	andi r3, r3, %lo(2390753280)
-	
-	or r3, r3, r2 #Mensagem de 32 bits completa
+	movia r3, 3254779904
 	
 	movi r4, 32 #Qtd de vezes que o loop deve ser realizado
 	movi r5, 0 #Contador
 	
-	movi r6, 1 #Numero de bits que devem ser deslocados a cada iteração
+	movi r6, 1 #Numero de bits que devem ser deslocados a cada iteraÃ§Ã£o
 	
 	movhi r7, %hi(2390753280) #Polinomio de 9 bits
 
