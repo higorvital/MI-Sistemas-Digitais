a3634d620e1adc02a1423e430bdd480a8180c134
higorvital <h.vitall96@gmail.com>
2018-04-02 09:56:44 -0300

    Update memory.s

--- memory.s
+++ memory.s
@@ -3,5 +3,5 @@ main:
 	
 	movi r3, 5
 	
-	stw r3, 0(r2) #Colocando dado de r3 na posição de memoria 0x00000810
-	ldw r4, 0(r2) #Pegando dado na posição de memoria 0x00000810 e colocando em r4
+	stw r3, 0(r2)
+	ldw r4, 0(r2)
