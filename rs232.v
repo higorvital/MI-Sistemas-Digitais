cef59be6caf828e9172483e8f12a68561fb90e46
higorvital <h.vitall96@gmail.com>
2018-07-30 16:26:35 +0000

    Update rs232.v

--- rs232.v
+++ rs232.v
@@ -5,8 +5,6 @@ MAIN:
 	movia r6, 0x0001
 	
 GET_CHAR:
-	subi sp, sp, 8
-	stw r5, 0(sp)
 	ldwio r2, 0(r4)
 	andi r5, r2, 0x8000
 	bne r5, r0, RETURN_CHAR
@@ -15,6 +13,4 @@ RETURN_CHAR:
 	andi r5, r2, 0x00ff
 	stw r2, 0(r6)
 	addi r6, r6, 4
-	ldw r5, 0(sp)
-	addi sp, sp, 8
 	ret
